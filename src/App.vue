59d822fc28427cb0ae8fd127d8113d26dc5045aa
vonaha <jon.victor.fortades2134@gmail.com>
2026-04-04 15:40:13 +0800

    Add a fade transition  when redirecting to Gallery

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade" mode="out-in">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
